a3799ab4d2d44b1ef528e2623e42ce6d8782bbfe
Stefan Prasse <prasse.stefan@gmx.de>
2014-08-10 16:44:09 +0200

    update version

--- TinyTinyRSS/Properties/AssemblyInfo.cs
+++ TinyTinyRSS/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // Sie können alle Werte angeben oder die Standardwerte für Revisions- und Buildnummer verwenden 
 // übernehmen, indem Sie "*" wie folgt verwenden:
-[assembly: AssemblyVersion("1.0.1.2")]
-[assembly: AssemblyFileVersion("1.0.1.2")]
+[assembly: AssemblyVersion("1.0.1.3")]
+[assembly: AssemblyFileVersion("1.0.1.3")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
