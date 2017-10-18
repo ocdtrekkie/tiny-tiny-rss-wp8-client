e572d952dd1f014d6200f130fdc10b4e164b1098
Stefan Prasse <prasse.stefan@gmx.de>
2017-10-18 22:34:02 +0200

    fall creators update

---
+++ ttrssClient/Classes/Article.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using Windows.ApplicationModel.Resources;
+using Windows.Graphics.Display;
+
+namespace TinyTinyRSS.Interface.Classes
+{
+    public sealed class Article : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int id { get; set; }
+        private bool _unread;
+        private bool _marked;
+        public string title { get; set; }
+        public string link { get; set; }
+        public object[] labels { get; set; }
+        public bool unread
+        {
+            get { return _unread; }
+            set
+            {
+                _unread = value;
+                OnPropertyChanged("unread");
+            }
+        }
+        public bool marked
+        {
+            get { return _marked; }
+            set
+            {
+                _marked = value;
+                OnPropertyChanged("marked");
+            }
+        }
+        public bool published { get; set; }
+        public string comments { get; set; }
+        public string author { get; set; }
+        public long updated { get; set; }
+        private string _content;
+        public string content
+        {
+            get
+            {
+                return _content;
+            }
+            set
+            {
+                ResourceLoader loader = new Windows.ApplicationModel.Resources.ResourceLoader();
+                ResolutionScale resolutionScale = DisplayInformation.GetForCurrentView().ResolutionScale;
+                double factor = (double)resolutionScale / 100.0;
+                double maxWidth = 340 * factor;
+                string colorStyle = "";
+                if (ConnectionSettings.getInstance().useDarkBackground)
+                {
+                    colorStyle = "html, body { color: #fff!Important; background-color: #000;} " +
+                        "a:link {color: #81DAF5;} ";
+                }
+                 _content = "<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+                     + "<style type=\"text/css\" title=\"text/css\">" + colorStyle +"</style>"
+                    + "<link href=\"ms-appx-web:///Strings/style.css\" rel=\"stylesheet\" /></head>"
+                    + "<body>" + value
+                    + "<br /><a href=\"" + link + "\">" + loader.GetString("LoadOriginalLink")
+                    + "</a></body></html>";
+            }
+        }
+        public int? feed_id { get; set; }
+        public object attachments { get; set; }
+        public int score { get; set; }
+        public string feed_title { get; set; }
+
+        // Create the OnPropertyChanged method to raise the event 
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ ttrssClient/ConnectionSettings.cs
@@ -0,0 +1,533 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using TinyTinyRSS.Classes;
+using Windows.ApplicationModel;
+using Windows.Foundation.Diagnostics;
+using Windows.Storage;
+
+namespace TinyTinyRSS.Interface
+{
+    public class ConnectionSettings
+    {
+        //Keys for settings
+        public static string _markReadKey = "AutoMarkRead";
+        public static string _serverKey = "Server";
+        public static string _usernameKey = "Username";
+        public static string _passwordKey = "PasswordNew";
+        public static string _showUnreadOnlyKey = "ShowUnreadOnly";
+        public static string _sortOrderKey = "SortOrder";
+        public static string _headlinesViewKey = "HeadlinesView";
+        public static string _progressBarAsCounterKey = "ProgressAsCntr";
+        public static string _liveTileActivatedKey = "LiveTileActive";
+        public static string _UseDarkBackgroundKey = "UseDarkBackground";
+        public static string _liveTileUpdateIntervalKey = "LiveTileUpdateInterval";
+        public static string _channelUriKey = "LiveTileUpdateChannel";
+        public static string _favFeedsKey = "FavoriteFeeds";
+        public static string _swipeMarginKey = "SwipeMargin";
+        public static string _allowSelfSignedKey = "AllowSelfSigned";
+        public static string _selectedFeedKey = "SelectedFeed";
+        public static string _suspensionDateKey = "SuspensionDateTime";
+        public static string _isCatKey = "IsSelectedFeedCategory";
+        public static string _lastLogKey = "lastLogFileName";
+        public static string _httpAuthKey = "httpAuth";
+        public static string _featuresVotedKey = "featureVote";
+        public static string _markReadByScrollingKey = "markReadByScrolling";
+
+
+        private static ConnectionSettings instance;
+        private string _server;
+        private string _username;
+        private string _password;
+        private string _markRead;
+        private string _unreadOnly;
+        private string _sortOrder;
+        private string _headlinesView;
+        private string _progressAsCntr;
+        private string _liveTileActivated;
+        private string _liveTileUpdateInterval;
+        private string _liveTileChannelUri;
+        private string _useDarkBackground;
+        private string _firstStart;
+        private string _favFeeds;
+        private string _swipeMargin;
+        private string _allowSelfSigned;
+        private string _selectedFeed;
+        private string _suspensionDate;
+        private string _isCategory;
+        private string _lastLog;
+        private string _httpAuth;
+        private string _featuresVoted;
+        private string _markReadByScrolling;
+        private LoggingChannel channel;
+
+        private ConnectionSettings()
+        {
+            channel = new LoggingChannel("Settings");
+            LogSession.addChannel(channel);
+        }
+
+        public static ConnectionSettings getInstance() {
+            if (instance == null)
+            {
+                instance = new ConnectionSettings();
+            }
+            return instance;
+        }
+
+        private static ApplicationDataContainer getLocalSettings()
+        {
+            return Windows.Storage.ApplicationData.Current.LocalSettings;
+        }
+
+        public string lastLog
+        {
+            get
+            {
+                if (_lastLog == null)
+                {
+                    _lastLog = ReadSetting(_lastLogKey);
+                }
+                return _lastLog;
+            }
+            set
+            {
+                SaveSetting(_lastLogKey, value);
+                _lastLog = value;
+            }
+        }
+
+        public string server
+        {
+            get
+            {
+                if (_server == null)
+                {
+                  _server = ReadSetting(_serverKey);
+                } 
+                return _server;
+            }
+            set
+            {
+                SaveSetting(_serverKey, value);
+                _server = value;
+            }
+        }
+
+        public string username
+        {
+            get
+            {
+                if (_username == null)
+                {
+                    _username = ReadSetting(_usernameKey);
+                } 
+                return _username;
+            }
+            set
+            {
+                SaveSetting(_usernameKey, value);
+                _username = value;
+            }
+        }
+
+        public string password
+        {
+            get
+            {
+                if (_password == null)
+                {
+                    _password = ReadSetting(_passwordKey);
+                }
+                return _password;
+            }
+            set
+            {
+                SaveSetting(_passwordKey, value);
+                _password = value;
+            }
+        }
+
+        public bool markRead
+        {
+            get
+            {
+                if (_markRead == null)
+                {
+                    _markRead = ReadSetting(_markReadKey);
+                } 
+                return _markRead.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(_markReadKey, value.ToString());
+                _markRead = value.ToString();
+            }
+        }
+        public bool markReadByScrolling
+        {
+            get
+            {
+                if (_markReadByScrolling == null)
+                {
+                    _markReadByScrolling = ReadSetting(_markReadByScrollingKey);
+                }
+                return _markReadByScrolling.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(_markReadByScrollingKey, value.ToString());
+                _markReadByScrolling = value.ToString();
+            }
+        }
+        
+        public bool httpAuth
+        {
+            get
+            {
+                if (_httpAuth == null)
+                {
+                    _httpAuth = ReadSetting(_httpAuthKey);
+                }
+                return _httpAuth.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(_httpAuthKey, value.ToString());
+                _httpAuth = value.ToString();
+            }
+        }
+
+        public bool featuresVoted
+        {
+            get
+            {
+                if (_featuresVoted == null)
+                {
+                    _featuresVoted = ReadSetting(featureKey);
+                }
+                return _featuresVoted.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(featureKey, value.ToString());
+                _featuresVoted = value.ToString();
+            }
+        }
+
+        private string featureKey
+        {
+            get
+            {
+                string key = string.Format("{0}_{1}.{2}",
+                    _featuresVotedKey,
+                    Package.Current.Id.Version.Major,
+                    Package.Current.Id.Version.Minor);
+                return key;
+            }
+        }
+        
+        public bool isCategory
+        {
+            get
+            {
+                if (_isCategory == null)
+                {
+                    _isCategory = ReadSetting(_isCatKey);
+                }
+                return _isCategory.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(_isCatKey, value.ToString());
+                _isCategory = value.ToString();
+            }
+        }
+
+        public bool firstStart
+        {
+            // don't use keys twice
+            // used: firstStartKey
+            get
+            {
+                if (_firstStart == null)
+                {
+                    _firstStart = ReadSetting("firstStartKeyFeatureRequests");
+                }
+                return !_firstStart.ToLower().Equals("false");
+            }
+            set
+            {
+                SaveSetting("firstStartKeyFeatureRequests", value.ToString());
+                _firstStart = value.ToString();
+            }
+        }
+
+        public bool liveTileActive
+        {
+            get
+            {
+                if (_liveTileActivated == null)
+                {
+                    _liveTileActivated = ReadSetting(_liveTileActivatedKey);
+                }
+                return _liveTileActivated.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(_liveTileActivatedKey, value.ToString());
+                _liveTileActivated = value.ToString();
+            }
+        }
+
+        public bool useDarkBackground
+        {
+            get
+            {
+                if (_useDarkBackground == null)
+                {
+                    _useDarkBackground = ReadSetting(_UseDarkBackgroundKey);
+                }
+                return _useDarkBackground.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(_UseDarkBackgroundKey, value.ToString());
+                _useDarkBackground = value.ToString();
+            }
+        }
+
+        public bool progressAsCntr
+        {
+            get
+            {
+                if (_progressAsCntr == null)
+                {
+                    _progressAsCntr = ReadSetting(_progressBarAsCounterKey);
+                }
+                return !_progressAsCntr.ToLower().Equals("false");
+            }
+            set
+            {
+                SaveSetting(_progressBarAsCounterKey, value.ToString());
+                _progressAsCntr = value.ToString();
+            }
+        }
+
+        public bool showUnreadOnly
+        {
+            get
+            {
+                if (_unreadOnly == null)
+                {
+                    _unreadOnly = ReadSetting(_showUnreadOnlyKey);
+                }
+                return _unreadOnly.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(_showUnreadOnlyKey, value.ToString());
+                _unreadOnly = value.ToString();
+            }
+        }
+
+        public bool allowSelfSignedCert
+        {
+            get
+            {
+                if (_allowSelfSigned == null)
+                {
+                    _allowSelfSigned = ReadSetting(_allowSelfSignedKey);
+                }
+                return _allowSelfSigned.ToLower().Equals("true");
+            }
+            set
+            {
+                SaveSetting(_allowSelfSignedKey, value.ToString());
+                _allowSelfSigned = value.ToString();
+            }
+        }
+
+        public int sortOrder
+        {
+            get
+            {
+                if (_sortOrder == null)
+                {
+                    _sortOrder = ReadSetting(_sortOrderKey);
+                    if (_sortOrder.Equals(""))
+                        _sortOrder = "0";
+                }
+                return int.Parse(_sortOrder);
+            }
+            set
+            {
+                SaveSetting(_sortOrderKey, value.ToString());
+                _sortOrder = value.ToString();
+            }
+        }
+
+        public int selectedFeed
+        {
+            get
+            {
+                if (_selectedFeed == null)
+                {
+                    _selectedFeed = ReadSetting(_selectedFeedKey);
+                    if (_selectedFeed.Equals(""))
+                        _selectedFeed = "-3";
+                }
+                return int.Parse(_selectedFeed);
+            }
+            set
+            {
+                SaveSetting(_selectedFeedKey, value.ToString());
+                _selectedFeed = value.ToString();
+            }
+        }
+
+        public int swipeMargin
+        {
+            get
+            {
+                if (_swipeMargin == null)
+                {
+                    _swipeMargin = ReadSetting(_swipeMarginKey);
+                    if (_swipeMargin.Equals(""))
+                        _swipeMargin = "5";
+                }
+                return int.Parse(_swipeMargin);
+            }
+            set
+            {
+                SaveSetting(_swipeMarginKey, value.ToString());
+                _swipeMargin = value.ToString();
+            }
+        }
+
+        public int tileUpdateInterval
+        {
+            get
+            {
+                if (_liveTileUpdateInterval == null)
+                {
+                    _liveTileUpdateInterval = ReadSetting(_liveTileUpdateIntervalKey);
+                    if (_liveTileUpdateInterval.Equals(""))
+                        _liveTileUpdateInterval = "0";
+                }
+                return int.Parse(_liveTileUpdateInterval);
+            }
+            set
+            {
+                SaveSetting(_liveTileUpdateIntervalKey, value.ToString());
+                _liveTileUpdateInterval = value.ToString();
+            }
+        }
+
+        public string channelUri
+        {
+            get
+            {
+                if (_liveTileChannelUri == null)
+                {
+                    _liveTileChannelUri = ReadSetting(_channelUriKey);
+                }
+                return _liveTileChannelUri;
+            }
+            set
+            {
+                SaveSetting(_channelUriKey, value);
+                _liveTileChannelUri = value;
+            }
+        }
+        
+        public DateTime supsensionDate {
+            get
+            {
+                if (_suspensionDate == null)
+                {
+                    _suspensionDate = ReadSetting(_suspensionDateKey);
+                }
+                if("".Equals(_suspensionDate)) {
+                    return DateTime.Now.AddMinutes(-15);
+                }
+                return DateTime.ParseExact(_suspensionDate, "F", CultureInfo.InvariantCulture);
+            }
+            set
+            {
+                SaveSetting(_suspensionDateKey, value.ToString("F", CultureInfo.InvariantCulture));
+                _suspensionDate = value.ToString("F", CultureInfo.InvariantCulture);
+            }
+        }
+
+        public HashSet<string> favFeeds
+        {
+            get
+            {
+                if (_favFeeds == null)
+                {
+                    _favFeeds = ReadSetting(_favFeedsKey);
+                }
+                string[] splitted = _favFeeds.Split(new char[] { ',' },StringSplitOptions.RemoveEmptyEntries);
+                return new HashSet<string>(splitted);
+            }
+        }
+
+        public void addFavFeed(string id)
+        {
+            if (_favFeeds == null || _favFeeds.Length == 0)
+            {
+                _favFeeds = id;
+            }
+            else
+            {
+                _favFeeds = _favFeeds + "," + id;
+            }
+            SaveSetting(_favFeedsKey, _favFeeds);
+        }
+
+        public void removeFavFeed(string id)
+        {
+            HashSet<string> local = new HashSet<string>(favFeeds);
+            if (local == null || local.Count == 0)
+            {
+                return;
+            }
+            else
+            {
+                local.Remove(id);
+            }
+            _favFeeds = string.Join(",", local.ToArray());
+            SaveSetting(_favFeedsKey, _favFeeds);
+        }
+
+        private void SaveSetting(string key, string value)
+        {
+            channel.LogMessage("Save setting " + key + " = " + value);
+            var values = getLocalSettings().Values;
+            if (!values.Keys.Contains(key))
+            {
+                values.Add(key, value);
+            }
+            else
+            {
+                values[key] = value;
+            }
+        }
+
+        private string ReadSetting(string key)
+        {
+            string setting;
+            if (getLocalSettings().Values.Keys.Contains(key))
+            {
+                setting = getLocalSettings().Values[key] as string;
+            } else
+            {
+                setting = "";
+            }
+            channel.LogMessage("Read setting " + key + " = " + setting);
+            return setting;
+        }
+    }
+}
